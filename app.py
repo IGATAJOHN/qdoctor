fa1e0e279a194a35b86f9143f00b5ab59ac94e38
IGATAJOHN <144548770+IGATAJOHN@users.noreply.github.com>
2025-01-09 01:32:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -716,7 +716,7 @@ def chatbot():
 def generate_response(conversation_history):
     try:
         completion = openai_client.chat.completions.create(
-            model="gpt-4-turbo",
+            model="gpt-4o",
             messages=[
                 {
                     "role": "system",
